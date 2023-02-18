e0cd6edcf5804d06869ed8ad4f3f454b6d6e5927
ayden <abaayd01@gmail.com>
2023-02-18 13:36:47 +1030

    improve tests for ast printer

--- ast_printer_test.go
+++ ast_printer_test.go
@@ -1,33 +1,65 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestAstPrinter_Print(t *testing.T) {
-	astPrinter := AstPrinter{}
-
-	l := Literal{value: 1}
-	r := Literal{value: 2}
-	plusToken := Token{
-		tokenType: PLUS,
-		lexeme:    "+",
-		literal:   nil,
-		line:      0,
+	type args struct {
+		expr Expr
 	}
-	b := Binary{
-		left:     l,
-		operator: plusToken,
-		right:    r,
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "basic",
+			args: args{
+				expr: Binary{
+					left: Literal{value: 1},
+					operator: Token{
+						tokenType: PLUS,
+						lexeme:    "+",
+						literal:   nil,
+						line:      0,
+					},
+					right: Literal{value: 2},
+				},
+			},
+			want: "(+ 1 2)",
+		},
+		{
+			name: "complex",
+			args: args{
+				expr: Binary{
+					left: Unary{
+						operator: Token{
+							tokenType: MINUS,
+							lexeme:    "-",
+							literal:   nil,
+							line:      0,
+						},
+						right: Literal{value: 123},
+					},
+					operator: Token{
+						tokenType: STAR,
+						lexeme:    "*",
+						literal:   nil,
+						line:      0,
+					},
+					right: Grouping{expression: Literal{value: 45.67}},
+				},
+			},
+			want: "(* (- 123) (group 45.67))",
+		},
 	}
-
-	res := astPrinter.Print(b)
-
-	expected := "(+ 1 2)"
-
-	if res != expected {
-		t.Errorf("result: %s, expected: %s", res, expected)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := AstPrinter{}
+			if got := a.Print(tt.args.expr); got != tt.want {
+				t.Errorf("Print() = %v, want %v", got, tt.want)
+			}
+		})
 	}
-	fmt.Println(res)
 }
